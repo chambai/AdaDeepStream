2f71ceea75c11b47314e4bdeee3e0eb958a999ab
chambai <61065458+chambai@users.noreply.github.com>
2023-12-21 13:20:48 +0000

    Add files via upload

--- modules_compare/rsb.py
+++ modules_compare/rsb.py
@@ -1,19 +1,19 @@
 import util
 from external.rsb.benchmark.er_runner_ds import DeepStreamRunner
-from core.dnn_models import DNNPytorchAdaptive
+from framework.dnn_models import DNNPytorchAdaptive
 import numpy as np
 
 dsr = None
 is_adapted = True # set to true as each cll adapts the DNN in this method
 
-def setupAnalysis(dnn, param_x_train_batch, param_y_train_batch):
+def setup(dnn, param_x_train_batch, param_y_train_batch):
     global dsr
     dnn_pt = DNNPytorchAdaptive()
     opt = dnn_pt.get_optimizer(dnn.model, util.getParameter('Dnn'))
     dsr = DeepStreamRunner(dnn.model, opt)
     dsr.fit(param_x_train_batch, param_y_train_batch)
 
-def processUnseenStreamBatch(batchDataInstances, batchTrueLabels):
+def adapt(batchDataInstances, batchTrueLabels):
     global dsr
     _, predictions = dsr.partial_fit(batchDataInstances, np.array(batchTrueLabels))
 
@@ -27,11 +27,11 @@ def processUnseenStreamBatch(batchDataInstances, batchTrueLabels):
     for t, p in zip(batchTrueLabels, predictions):
         true_labels_str += str(t)
         predict_str += str(p)
-    util.thisLogger.logInfo('TRUE LABELS: %s' % (true_labels_str))
-    util.thisLogger.logInfo('PREDICTED:   %s' % (predict_str))
+    # util.thisLogger.logInfo('TRUE LABELS: %s' % (true_labels_str))
+    # util.thisLogger.logInfo('PREDICTED:   %s' % (predict_str))
     correct = [y for i, y in enumerate(predictions) if batchTrueLabels[i] == int(y)]
     acc = len(correct) / len(batchTrueLabels)
-    util.thisLogger.logInfo('DNN_Predict Acc: %s' % (acc))
+    # util.thisLogger.logInfo('DNN_Predict Acc: %s' % (acc))
 
     for i in range(len(batchDataInstances)):
         batchDiscrepResult.append('N')
